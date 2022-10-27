0b31f6361df3337f86a4763a36f0ed3f12e69aa6
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-10-27 15:05:15 -0300

    Delete Task_05_27.10/API (tem typeorm) directory

--- Task_05_27.10/API (tem typeorm)/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { syncBuiltinESMExports } from 'module';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Fórum Dev Girls!';
-  }
-}
--- Task_05_27.10/API (tem typeorm)/src/auth/entities/usuariologin.entity.ts
+++
@@ -1,6 +0,0 @@
-export class UsuarioLogin {
-
-    usuario: string
-
-    senha: string
-}
