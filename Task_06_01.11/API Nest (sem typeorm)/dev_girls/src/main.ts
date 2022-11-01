7399e40bd90c6b3936cd689af6e849b526821d7a
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2022-11-01 16:25:26 -0300

    Add files via upload

---
+++ Task_06_01.11/API Nest (sem typeorm)/dev_girls/src/main.ts
@@ -0,0 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fórum Dev Girls')
+    .setDescription('Projeto do Fórum Dev Girls')
+    .setContact('Generation Brasil', 'www.genbr.com.br', 'gstvgeneration@gmail.com')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('/swagger', app, document)
+
+  process.env.TZ = '-03:00'
+  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors()
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
